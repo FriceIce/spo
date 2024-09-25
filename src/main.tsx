c10cb0f0fb9d2cab99deee21ce4d184d83989213
Isak Tilahun <Tilahun.Isak@gmail.com>
2024-09-25 18:47:51 +0200

    Implemented React query and also Redux toolkit

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+// Import your custom hooks and components here
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { store } from "./redux/store.ts";
+import { Provider } from "react-redux";
+
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </Provider>
+  </StrictMode>
+);
