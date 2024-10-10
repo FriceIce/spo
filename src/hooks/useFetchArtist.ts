4b2ee9ee7c1ae3e4ae71ac57dc20950f9b0cd0ab
Isak Tilahun <Tilahun.Isak@gmail.com>
2024-10-10 12:28:28 +0200

    Created custom hook for fetching with useQueries. By doing this the amount of code in the Artist component is reduced by alot.

---
+++ src/hooks/useFetchArtist.ts
@@ -0,0 +1,118 @@
+import { useQueries, useQueryClient } from "@tanstack/react-query";
+import useSpotify from "./useSpotify";
+
+const useFetchArtist = (id: string) => {
+  const { spotifyApi } = useSpotify();
+
+  const queryClient = useQueryClient();
+  const currentArtist = queryClient.getQueryData([
+    "Artist-" + id,
+  ]) as SpotifyApi.SingleArtistResponse;
+  const tracks = queryClient.getQueryData([
+    "ArtistTopTracks-" + id,
+  ]) as SpotifyApi.TrackObjectFull[];
+  const artistAlbums = queryClient.getQueryData([
+    "ArtistAlbums-" + id,
+  ]) as SpotifyApi.AlbumObjectSimplified[];
+  const relArtists = queryClient.getQueryData([
+    "ArtistRelatedArtists-" + id,
+  ]) as SpotifyApi.ArtistObjectFull[];
+  const artistRelPlaylists = queryClient.getQueryData([
+    "ArtistRelatedPlaylists-" + id,
+  ]) as SpotifyApi.PlaylistObjectSimplified[];
+
+  const queries = useQueries({
+    queries: [
+      {
+        enabled: !currentArtist,
+        initialData: currentArtist,
+        queryKey: ["Artist-" + id],
+        queryFn: async () =>
+          spotifyApi.getArtist(id ?? "").then((result) => {
+            return result;
+          }),
+      },
+      {
+        enabled: !tracks,
+        initialData: tracks,
+        queryKey: ["ArtistTopTracks-" + id],
+        queryFn: async () =>
+          spotifyApi.getArtistTopTracks(id ?? "", "ES").then((result) => {
+            return result.tracks;
+          }),
+      },
+      {
+        enabled: !artistAlbums,
+        initialData: artistAlbums,
+        queryKey: ["ArtistAlbums-" + id],
+        queryFn: async () =>
+          spotifyApi
+            .getArtistAlbums(id ?? "", { market: "ES" })
+            .then((result) => {
+              return result.items;
+            }),
+      },
+      {
+        enabled: !relArtists,
+        initialData: relArtists,
+        queryKey: ["ArtistRelatedArtists-" + id],
+        queryFn: async () =>
+          spotifyApi.getArtistRelatedArtists(id ?? "").then((result) => {
+            return result.artists;
+          }),
+      },
+      {
+        enabled: !artistRelPlaylists,
+        initialData: artistRelPlaylists,
+        queryKey: ["ArtistRelatedPlaylists-" + id],
+        queryFn: async () =>
+          spotifyApi
+            .getArtist(id ?? "")
+            .then((result) => {
+              return result;
+            })
+            .then((result) => {
+              const query = result.name;
+              return spotifyApi
+                .searchPlaylists(query, {
+                  type: "playlist",
+                  market: "ES",
+                  limit: 10,
+                })
+                .then((result) => {
+                  const owner = "spotify";
+                  const onlySpotifyPlaylists = result.playlists.items.filter(
+                    (playlist) =>
+                      playlist.owner.display_name?.toLocaleLowerCase() === owner
+                  );
+                  return onlySpotifyPlaylists;
+                });
+            }),
+      },
+    ],
+  });
+
+  const artist = queries[0].data as SpotifyApi.SingleArtistResponse;
+  const topTracks = queries[1].data as SpotifyApi.TrackObjectFull[];
+  const albums = queries[2].data as SpotifyApi.AlbumObjectSimplified[];
+  const relatedArtists = queries[3].data as SpotifyApi.ArtistObjectFull[];
+  const artistRelatedPlaylists = queries[4]
+    .data as SpotifyApi.PlaylistObjectSimplified[];
+
+  const isLoading = queries.find((query) => query.isLoading);
+  const isSuccess = queries.find((query) => query.isSuccess);
+  const isError = queries.find((query) => query.isError);
+
+  return {
+    artist,
+    topTracks,
+    albums,
+    relatedArtists,
+    artistRelatedPlaylists,
+    isLoading,
+    isSuccess,
+    isError,
+  };
+};
+
+export default useFetchArtist;
